7499c5102029fddd97458e2898b07907fe74caca
Huynh Uyen <uyenhuynh13122000@gmail.com>
2023-12-14 14:30:06 +0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkpurple: "#130f1c",
+        lightpurple: "#7863e0",
+        gray1: "#dddddd",
+        gray2: "#999999",
+        line: "#27213b",
+      },
+    },
   },
   plugins: [],
 };
